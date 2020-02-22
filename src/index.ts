7076c0d52a2663a691a28236decb28e557004b65
Ilnaz Valiev <ilnazv90@gmail.com>
2020-02-21 17:45:13 -0800

    ts support

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "./style.scss";
+
+function initialize(): void {
+    const string = 'Hello, world';
+    console.log(string);
+}
+
+initialize();
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,40 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    app: './src/index.js'
+    app: "./src/index.ts"
   },
-  devtool: 'inline-source-map',
-   devServer: {
-     contentBase: './dist',
-   },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: [".ts", ".tsx", ".js"]
+  },
   plugins: [
     // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Development',
-      template: 'src/index.html'
-    }),
+      title: "Development",
+      template: "src/index.html"
+    })
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
-  module:{
-    rules:[
-            {
-                test:/\.(s*)css$/,
-                use:['style-loader','css-loader', 'sass-loader']
-             }
-     ]
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ]
   }
 };
